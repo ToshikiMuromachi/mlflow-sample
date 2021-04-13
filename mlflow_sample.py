dc74b948814c79842817be819b820bb905a41d11
Toshiki Muromachi <ageki104@gmail.com>
2021-04-13 12:33:52 +0900

    Initial commit

---
+++ mlflow_sample.py
@@ -0,0 +1,19 @@
+import mlflow
+
+# runIDを発行する
+mlflow.start_run()
+
+# パラメータを記録する (key-value pair)
+mlflow.log_param("param1", 5)
+
+# メトリックを記録する(ステップごとに)
+mlflow.log_metric("foo", 2, step=1)
+mlflow.log_metric("foo", 4, step=2)
+mlflow.log_metric("foo", 6, step=3)
+
+# 生成物を記録する (output file)
+with open("output.txt", "w") as f:
+    f.write("Hello world!")
+mlflow.log_artifact("output.txt")
+
+mlflow.end_run()
